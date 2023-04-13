fdd056b0812d29299bd9a349aecc0188f9a455a9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-04-13 21:19:21 +0200

    feat: Terminated exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut split = s.split(',');
+        let name = split.next();
+        let age = split.next();
+        if split.next() != None { return Person::default(); }
+        if name == None || age == None {
+            return Person::default();
+        }
+        let name_parsed = name.unwrap().to_string();
+        if name_parsed == "" { return Person::default(); }
+        let mut age_parsed;
+        if let Ok(result) = age.unwrap().parse::<usize>() {
+            age_parsed = result;
+        } else {
+            return Person::default(); 
+        }
+        Person { name: name_parsed, age: age_parsed }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {return Err(ParsePersonError::Empty);}
+        let mut split = s.split(',');
+        let name = split.next();
+        let age = split.next();
+        if name == None || name.unwrap() == "" {return Err(ParsePersonError::NoName);}
+        if age == None || split.next() != None {return Err(ParsePersonError::BadLen);}
+        let parsed_name = name.unwrap().to_string();
+        let mut parsed_age;
+        match age.unwrap().parse::<usize>() {
+            Ok(result) => parsed_age = result,
+            Err(error) => return Err(ParsePersonError::ParseInt(error))
+        }
+        Ok(Person {
+            name: parsed_name,
+            age: parsed_age,
+        })
     }
 }
 
